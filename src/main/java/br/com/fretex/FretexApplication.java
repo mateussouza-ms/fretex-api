8e25d08dd19614aef01ea6f86830057f33454e35
Mateus Souza <mateus_souza_teles@hotmail.com>
2020-09-17 16:17:25 -0300

    Criar o projeto
    Foi criado um projeto Spring Boot com as configuraçãoes e os starters iniciais.

---
+++ src/main/java/br/com/fretex/FretexApplication.java
@@ -0,0 +1,13 @@
+package br.com.fretex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FretexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FretexApplication.class, args);
+	}
+
+}
