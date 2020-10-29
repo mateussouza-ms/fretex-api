1be0ec25878569ac0778f21c7b6115c6785b5cc6
Mateus Souza <mateus_souza_teles@hotmail.com>
2020-10-29 00:21:54 -0300

    Adequar a segurança para cadastro de novos usuários
    Adequação para que seja permitida a chamada a API de cadastro de novo usuário, sem a necessidade de estar autenticado.
    [INCLUSÃO DE DUAS CLASSES QUE FICARAM FORA DO COMMIT ANTERIOR POR ENGANO]

---
+++ src/main/java/br/com/fretex/config/JpaUserDetailsService.java
@@ -0,0 +1,58 @@
+package br.com.fretex.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.fretex.domain.model.Usuario;
+import br.com.fretex.domain.repository.ClienteRepository;
+import br.com.fretex.domain.repository.PrestadorServicoRepository;
+import br.com.fretex.domain.repository.UsuarioRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private PrestadorServicoRepository prestadorServicoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> usuario = usuarioRepository.findByEmail(username);
+
+		if (!usuario.isPresent()) {
+			usuario = usuarioRepository.findByCnp(username);
+		}
+
+		if (!usuario.isPresent()) {
+			System.out.println("UsernameNotFoundException");
+			throw new UsernameNotFoundException("Usuário não encontrado com o e-mail informado");
+		}
+
+		List<GrantedAuthority> authorities = new ArrayList<>();
+
+		if (prestadorServicoRepository.findByUsuario(usuario.get()).isPresent()) {
+			authorities.add(new SimpleGrantedAuthority(ConstPerfil.PRESTADOR_SERVICOS));
+		}
+
+		if (clienteRepository.findByUsuarioId(usuario.get().getId()).isPresent()) {
+			authorities.add(new SimpleGrantedAuthority(ConstPerfil.CLIENTE));
+		}
+
+		return new AuthUser(usuario.get(), authorities);
+	}
+
+}
